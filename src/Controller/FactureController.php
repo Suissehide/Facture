9f77dec1712abb13bfd85093f3d85f5eb4d54d63
leo.couffinhal1@gmail.com <leo.couffinhal@epitech.eu>
2019-06-17 15:04:30 +0200

    push

--- src/Controller/FactureController.php
+++ src/Controller/FactureController.php
@@ -14,23 +14,55 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 class FactureController extends AbstractController
 {
     /**
-     * @Route("/facture", name="facture")
+     * @Route("/", name="index")
      */
     public function index(Request $request)
+    {
+        return $this->render('facture/index.html.twig', [
+            'controller_name' => 'FactureController',
+        ]);
+    }
+
+    /**
+     * @Route("/facture", name="new_facture")
+     */
+    public function new(Request $request)
     {
         $facture = new Facture();
         $form = $this->createForm(FactureType::class, $facture);
         $em = $this->getDoctrine()->getManager();
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            if ($form->get('validation')->isClicked()) {
+            if ($form->get('save')->isClicked()) {
                 $facture = $form->getData();
                 $em->flush();
             }
             return $this->redirect($request->getUri());
         }
 
-        return $this->render('facture/index.html.twig', [
+        return $this->render('facture/edit.html.twig', [
+            'controller_name' => 'FactureController',
+            'form' => $form->createView(),
+        ]);
+    }
+
+        /**
+     * @Route("/facture/{id}", name="edit_facture")
+     */
+    public function edit(Request $request, Facture $facture)
+    {
+        $form = $this->createForm(FactureType::class, $facture);
+        $em = $this->getDoctrine()->getManager();
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($form->get('save')->isClicked()) {
+                $facture = $form->getData();
+                $em->flush();
+            }
+            return $this->redirect($request->getUri());
+        }
+
+        return $this->render('facture/edit.html.twig', [
             'controller_name' => 'FactureController',
             'form' => $form->createView(),
         ]);
--- src/Form/FactureType.php
+++ src/Form/FactureType.php
@@ -6,20 +6,53 @@ use App\Entity\Facture;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 
 class FactureType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('myCompany')
-            ->add('clientCompany')
+            ->add('myCompany', TextType::class, array(
+                'attr' => array(
+                    'placeholder' => 'Mon entreprise',
+                )
+            ))
+            ->add('clientCompany', TextType::class, array(
+                'attr' => array(
+                    'placeholder' => 'Entreprise du client',
+                )
+            ))
             ->add('total')
             ->add('VAT')
-            ->add('invoiceDate')
-            ->add('due_date')
+            ->add('invoiceDate', DateType::class, array(
+                'label' => 'Date de facturation',
+                'widget' => 'single_text',
+                'format' => 'dd/MM/yyyy',
+                'required' => true,
+                'attr' => array(
+                    'placeholder' => 'Date de facturation',
+                    'data-language' => 'fr',
+                )
+            ))
+            ->add('dueDate', DateType::class, array(
+                'label' => 'Date d\'échéance',
+                'widget' => 'single_text',
+                'format' => 'dd/MM/yyyy',
+                'required' => true,
+                'attr' => array(
+                    'data-language' => 'fr'
+                )
+            ))
             ->add('projectDescription')
-            ->add('paymentMethods')
+            ->add('paymentMethods', ChoiceType::class, array(
+                'choices' => array(
+                    'Carte de crédit' => 'Carte de crédit',
+                    'PayPal' => 'PayPal',
+                ),
+            ))
             ->add('details')
             ->add('invoiceNumber')
             ->add('referenceNo')
